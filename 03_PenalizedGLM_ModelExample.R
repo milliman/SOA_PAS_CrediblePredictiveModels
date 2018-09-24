4953a143ddbb19a1cdbcf124e0bce7bae414e0ef
MKullowatz <matthias.kullowatz@milliman.com>
2018-09-23 21:49:48 -0700

    Preserve all levels of categorical variables for model matrix

--- 03_PenalizedGLM_ModelExample.R
+++ 03_PenalizedGLM_ModelExample.R
@@ -1,7 +1,49 @@
-library(glmnet)
+# Fit the same model, but shrink the distributor codes using penalization
+
+# library(glmnet)
 
 # Read data ####
 modeldata <- readRDS("SampleDataset.RDS")
 
+# Make "IN" (whether the policyholder is in surrender charge period) categorical
+modeldata <- modeldata %>%
+  mutate(IN = factor(IN))
+
+# Prepare model matrix ####
+x.train <- model.matrix(object = ~ IN + Dur_IN + ITM:Dur_IN + Dur_OUT + ITM:Dur_OUT + DistCode,
+                  data = modeldata %>% 
+                    filter(Sample == "training"),
+                  contrasts.arg = lapply(modeldata %>% 
+                                           filter(Sample == "training") %>% 
+                                           select(IN, DistCode),
+                                         contrasts,
+                                         contrasts = FALSE))
+
+x.train <- x.train[,-1] # remove the intercept column
+
 # Fit penalized GLM ####
-x <- model.matrix(~ IN + Dur_IN + ITM:Dur_IN + Dur_OUT + ITM:Dur_OUT + DistCode)
+p.fac <- ifelse(1:ncol(x.train) %in% grep("DistCode", colnames(x.train)), 1, 0) # select which columns to be shrunk
+
+# Elastic net fit ####
+# Takes 5 - 6 minutes
+system.time(
+  cv.glmnet.model <- cv.glmnet(x = x.train,
+                               y = modeldata$Surr[modeldata$Sample == "training"],
+                               family = "binomial",
+                               penalty.factor = p.fac,
+                               standardize = T,
+                               alpha = 0.5, # 50% LASSO and 50% Ridge penalties
+                               nfolds = 5
+                               #,
+                               #offset = OFFSET_COLNAME) # this is where you input your offset
+  )
+)
+
+plot.cv.glmnet(cv.glmnet.model, sign.lambda=-1)
+
+
+# Summarize DistCode coefficients ####
+C <- coef(cv.glmnet.model, s = "lambda.min")
+summary(C[grep("DistCode", rownames(C)),])
+
+
