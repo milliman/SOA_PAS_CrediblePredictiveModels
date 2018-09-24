53fe238ffad62777681b37d09f8eb96d11f52a70
MKullowatz <matthias.kullowatz@milliman.com>
2018-09-23 21:48:05 -0700

    Load packages on master script

--- 00_MasterScript.R
+++ 00_MasterScript.R
@@ -1,4 +1,10 @@
 # Order of scripts
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+library(lme4)
+library(glmnet)
+
 source("01_CreateStylizeddata.R")
 source("02_LME_ModelExample.R")
 source("02b_LME_ModelValidation.R")
